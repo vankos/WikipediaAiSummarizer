9015624a796ef09dfc7181fa7df36da017cc9a04
vankos <14087195+vankos@users.noreply.github.com>
2025-08-19 20:08:44 +0300

    add asking for image

--- app/src/main/java/com/example/wikipediaaisummarizer/ui/PromptService.kt
+++ app/src/main/java/com/example/wikipediaaisummarizer/ui/PromptService.kt
@@ -10,16 +10,18 @@ class PromptService {
             "2. Describe the most interesting thing about the subject in the text below.\n" +
             "3. If there’s a notable story connected to the subject, tell it.\n" +
             "4. Feel free to include any additional information about the subject (or related stories) beyond the provided text if you know it.\n" +
-            "5. Provide a good source about the subject (besides a Wikipedia article) if available—a book, film, or article is fine.\n" +
+            "5. Show relevant to topic image if possible .\n" +
+            "6. Provide a good source about the subject (besides a Wikipedia article) if available—a book, film, or article is fine.\n" +
             "Here’s the text: $content"
 
     fun getPromptWithTopicName(topic: String?) = "Write the response in " +
             "${Locale.getDefault().getDisplayLanguage(Locale("en"))}." +
             "Tell me about $topic\n" +
             "1. Explain what the subject is known for.\n" +
-            "2. Describe the most interesting thing about the subject in the text below.\n" +
+            "2. Describe the most interesting thing about the subject.\n" +
             "3. If there’s a notable story connected to the subject, tell it.\n" +
             "4. Feel free to include any additional information about the subject (or related stories) beyond the provided text if you know it.\n" +
-            "5. Provide a good source about the subject (besides a Wikipedia article) if available—a book, film, or article is fine."
+            "5. Show relevant to topic image if possible .\n" +
+            "6. Provide a good source about the subject (besides a Wikipedia article) if available—a book, film, or article is fine."
 
 }
