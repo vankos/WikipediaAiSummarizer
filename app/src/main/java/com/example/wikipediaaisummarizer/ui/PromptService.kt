a19c1daab1f99c70a6ecd9c79a861254569ef1b2
vankos <14087195+vankos@users.noreply.github.com>
2025-10-28 21:57:26 +0300

    add promptTemplate

--- app/src/main/java/com/example/wikipediaaisummarizer/ui/PromptService.kt
+++ app/src/main/java/com/example/wikipediaaisummarizer/ui/PromptService.kt
@@ -4,21 +4,18 @@ import java.util.Locale
 
 class PromptService {
 
-    fun getPrompt(content: String?) = "Write the response in " +
-            "${Locale.getDefault().getDisplayLanguage(Locale("en"))}. " +
-            "1. Explain what the subject is known for.\n" +
-            "2. Describe the most interesting thing about the subject in the text below.\n" +
-            "3. If there’s a notable story connected to the subject, tell it.\n" +
-            "4. What’s the most meme-worthy or funniest aspect of the subject, if any?\n" +
-            "5. Feel free to include any additional information about the subject (or related stories) beyond the provided text if you know it.\n" +
+    val promptTemplate : String = "\"Write the response in \" +\n" +
+            "            \"${Locale.getDefault().getDisplayLanguage(Locale("en"))}. \" +\n" +
+            "            \"1. Explain what the subject is known for.\\n\" +\n" +
+            "            \"2. Describe the most interesting thing about the subject.\\n\" +\n" +
+            "            \"3. If there’s a notable story connected to the subject, tell it.\\n\" +\n" +
+            "            \"4. What’s the most meme-worthy or funniest aspect of the subject, if any?\\n\" +\n" +
+            "            \"5. Feel free to include any additional information about the subject (or related stories) if you know it.\\n\" +"
+
+    fun getPrompt(content: String?) = promptTemplate +
             "Here’s the text: $content"
 
-    fun getPromptWithTopicName(topic: String?) = "Write the response in " +
-            "${Locale.getDefault().getDisplayLanguage(Locale("en"))}." +
+    fun getPromptWithTopicName(topic: String?) = 
             "Tell me about $topic\n" +
-            "1. Explain what the subject is known for.\n" +
-            "2. Describe the most interesting thing about the subject.\n" +
-            "3. If there’s a notable story connected to the subject, tell it.\n" +
-            "4. What’s the most meme-worthy or funniest aspect of the subject, if any?\n" +
-            "5. Feel free to include any additional information about the subject (or related stories) if you know it.\n"
+            promptTemplate
 }
